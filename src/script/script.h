6f7094766905696c404b272d0b6fb46660a2ee4f
AOK21 <dev@aok21.com>
2020-05-26 04:37:54 +0900

    Updated missing files

--- src/script/script.h
+++ src/script/script.h
@@ -548,6 +548,7 @@ public:
     bool IsPayToWitnessScriptHash() const;
     bool IsPayToWitnessPubkeyHash() const;
     bool IsWitnessProgram(int& version, std::vector<unsigned char>& program) const;
+    bool IsPayToPublicKey() const;
 
     /** Called by IsStandardTx and P2SH/BIP62 VerifyScript (which makes it consensus-critical). */
     bool IsPushOnly(const_iterator pc) const;
