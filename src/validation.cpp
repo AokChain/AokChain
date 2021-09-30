fc6ed949aca5e869408d637c946e2c64e6344bff
volbil <volbilnexus@gmail.com>
2021-09-30 15:17:02 +0900

    Updated strMessageMagic

--- src/validation.cpp
+++ src/validation.cpp
@@ -114,7 +114,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const std::string strMessageMagic = "BlackCoin Signed Message:\n";
+const std::string strMessageMagic = "AOK Signed Message:\n";
 
 // Internal stuff
 namespace {
